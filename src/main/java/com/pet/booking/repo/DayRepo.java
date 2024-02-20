d30b61d323d75490616ab0a3d6cf766274865a1b
dmitriychervanev <junktoun@gmail.com>
2024-02-20 21:14:48 +0200

    Deleted Day with relations

--- src/main/java/com/pet/booking/repo/DayRepo.java
+++
@@ -1,7 +0,0 @@
-package com.pet.booking.repo;
-
-import com.pet.booking.models.bookingCalendar.Day;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DayRepo extends CrudRepository<Day, Long> {
-}
